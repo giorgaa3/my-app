350d4b8ea056012c798d68f4e8c3807bf1606621
NIKOLOZI GIORGADZE <nikagiorgadze2004@gmail.com>
2026-04-24 15:50:48 +0400

    layout report fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased">
-      <body className="min-h-full bg-background text-foreground">
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body
+        className="min-h-full bg-background text-foreground"
+        suppressHydrationWarning
+      >
         {children}
       </body>
     </html>
