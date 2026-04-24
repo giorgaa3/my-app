57dbfbafeeb7667ce73f691d80faaec8426d9235
NIKOLOZI GIORGADZE <nikagiorgadze2004@gmail.com>
2026-04-24 15:14:37 +0400

    adding icon

--- components/ui/Icon.tsx
+++ components/ui/Icon.tsx
@@ -1,6 +1,21 @@
 import type { SVGProps } from "react";
 
 const iconPaths = {
+  alert: (
+    <>
+      <path d="M12 9v4" />
+      <path d="M12 17h.01" />
+      <path d="M10.3 3.8 2.7 17a2 2 0 0 0 1.7 3h15.2a2 2 0 0 0 1.7-3L13.7 3.8a2 2 0 0 0-3.4 0Z" />
+    </>
+  ),
+  calendar: (
+    <>
+      <path d="M8 3v4" />
+      <path d="M16 3v4" />
+      <path d="M4 9h16" />
+      <path d="M6 5h12a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2Z" />
+    </>
+  ),
   check: <path d="m5 12 4 4L19 6" />,
   checkCircle: (
     <>
@@ -9,6 +24,18 @@ const iconPaths = {
     </>
   ),
   circle: <circle cx="12" cy="12" r="8" />,
+  edit: (
+    <>
+      <path d="M12 20h9" />
+      <path d="M16.5 3.5a2.1 2.1 0 0 1 3 3L8 18l-4 1 1-4 11.5-11.5Z" />
+    </>
+  ),
+  flag: (
+    <>
+      <path d="M5 21V4" />
+      <path d="M5 4h12l-2 5 2 5H5" />
+    </>
+  ),
   flame: (
     <path d="M12 21c-3.4 0-6-2.5-6-5.8 0-2.3 1.3-4.2 3.1-5.6.9-.7 1.7-1.7 1.9-3.3 2.3 1.1 4 3.1 4.4 5.4.6-.4 1.1-1 1.4-1.7 1 1 1.7 2.8 1.7 4.7 0 3.7-2.7 6.3-6.5 6.3Z" />
   ),
@@ -22,16 +49,51 @@ const iconPaths = {
       <path d="M4 18h.01" />
     </>
   ),
+  moon: (
+    <path d="M21 14.5A8.5 8.5 0 0 1 9.5 3 7 7 0 1 0 21 14.5Z" />
+  ),
   plus: (
     <>
       <path d="M12 5v14" />
       <path d="M5 12h14" />
     </>
   ),
+  reset: (
+    <>
+      <path d="M4 12a8 8 0 1 0 2.3-5.7" />
+      <path d="M4 4v6h6" />
+    </>
+  ),
+  save: (
+    <>
+      <path d="M5 3h12l2 2v16H5V3Z" />
+      <path d="M9 3v6h6V3" />
+      <path d="M8 21v-7h8v7" />
+    </>
+  ),
+  search: (
+    <>
+      <circle cx="11" cy="11" r="7" />
+      <path d="m20 20-3.5-3.5" />
+    </>
+  ),
   spark: (
     <>
       <path d="m12 3 1.7 5.1L19 10l-5.3 1.9L12 17l-1.7-5.1L5 10l5.3-1.9L12 3Z" />
       <path d="m19 15 .7 2.1L22 18l-2.3.9L19 21l-.7-2.1L16 18l2.3-.9L19 15Z" />
+    </>
+  ),
+  sun: (
+    <>
+      <circle cx="12" cy="12" r="4" />
+      <path d="M12 2v2" />
+      <path d="M12 20v2" />
+      <path d="m4.9 4.9 1.4 1.4" />
+      <path d="m17.7 17.7 1.4 1.4" />
+      <path d="M2 12h2" />
+      <path d="M20 12h2" />
+      <path d="m4.9 19.1 1.4-1.4" />
+      <path d="m17.7 6.3 1.4-1.4" />
     </>
   ),
   target: (
@@ -57,6 +119,12 @@ const iconPaths = {
     <>
       <path d="M4 18 10 12l4 4 6-8" />
       <path d="M15 8h5v5" />
+    </>
+  ),
+  x: (
+    <>
+      <path d="M18 6 6 18" />
+      <path d="m6 6 12 12" />
     </>
   ),
 } as const;
